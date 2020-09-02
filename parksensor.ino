2bf7a06685d6ad57ca42a0fd153bfe2d5b39c1be
Furkan Alp Tokaç <furkanalptokac@gmail.com>
2020-09-02 16:09:35 +0300

    clean code, functions

--- parksensor.ino
+++ parksensor.ino
@@ -2,8 +2,69 @@ int buzzer = 2;
 int trigPin = 11;
 int echoPin = 12;
 
-long sure;
-long uzaklik;
+int d1 = 20;
+int d2 = 15;
+int d3 = 10;
+int d4 = 5;
+
+long duration;
+long distance;
+
+void distance1 (long d) {
+  d = d1;
+  for (int i = 3; i <= 10; i++)
+    digitalWrite(i, LOW);
+      
+  digitalWrite(buzzer, LOW);
+}
+
+void distance2 () {
+  digitalWrite(3, HIGH);
+  digitalWrite(10, HIGH);
+  
+  for (int i = 4; i <= 9; i++)
+    digitalWrite(i, LOW);
+      
+  digitalWrite(buzzer, HIGH);
+  delay(100);
+  digitalWrite(buzzer, LOW);
+  delay(600);
+}
+
+void distance3 () {
+  for (int i = 3; i <= 4; i++)
+    digitalWrite(i, HIGH);
+  for (int i = 9; i <= 10; i++)
+    digitalWrite(i, HIGH);
+  for (int i = 5; i <= 8; i++)
+    digitalWrite(i, LOW);
+      
+  digitalWrite(buzzer, HIGH);
+  delay(100);
+  digitalWrite(buzzer, LOW);
+  delay(200);
+}
+
+void distance4 () {
+  for (int i = 3; i <= 5; i++)
+    digitalWrite(i, HIGH);
+  for (int i = 6; i <= 7; i++)
+    digitalWrite(i, LOW);  
+  for (int i = 8; i <= 10; i++)
+    digitalWrite(i, HIGH);
+  
+    digitalWrite(buzzer, HIGH);
+    delay(100);
+    digitalWrite(buzzer, LOW);
+    delay(70);
+}
+
+void distance5 () {
+  for (int i = 3; i <= 10; i++)
+    digitalWrite(i, HIGH);
+      
+    digitalWrite(buzzer, HIGH);
+}
 
 void setup() {
   pinMode(buzzer, OUTPUT);
@@ -12,7 +73,7 @@ void setup() {
 
   for(int i = 3; i <= 10; i++)
     pinMode(i, OUTPUT);
-
+    
   Serial.begin(9600);
 }
 
@@ -22,82 +83,31 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  sure = pulseIn(echoPin, HIGH);
-  uzaklik = sure / 29.1 / 2;
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration / 29.1 / 2;
   
-  if (uzaklik > 20) {
-    uzaklik = 20;
-    digitalWrite(3, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(buzzer, LOW);
-  }
-    
-  
-  if (uzaklik < 20 && uzaklik >= 15) {
-    digitalWrite(3, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(buzzer, HIGH);
-    delay(100);
-    digitalWrite(buzzer, LOW);
-    delay(600);
+  if (distance > d1) {
+    distance1(distance);
   }
 
-  else if (uzaklik < 15 && uzaklik >= 10) {
-    digitalWrite(3, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(9, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(100);
-    digitalWrite(buzzer, LOW);
-    delay(200);
+  if (distance < d1 && distance >= d2) {
+    distance2();
   }
 
-  else if (uzaklik < 10 && uzaklik >= 5) {
-    digitalWrite(3, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    digitalWrite(9, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(100);
-    digitalWrite(buzzer, LOW);
-    delay(70);
+  else if (distance < d2 && distance >= d3) {
+    distance3();
   }
 
-  else if (uzaklik < 5) {
-    digitalWrite(3, HIGH);
-    digitalWrite(10, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(6, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
-    digitalWrite(9, HIGH);
-    digitalWrite(buzzer, HIGH);
+  else if (distance < d3 && distance >= d4) {
+    distance4();
   }
-  
-  Serial.print("Uzaklik ");
-  Serial.print(uzaklik);
+
+  else if (distance < d4) {
+    distance5();
+  }
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
   Serial.println(" cm");
   delay(100);
 }
